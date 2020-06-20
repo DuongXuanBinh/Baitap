32d972483a68800af6c2e352c1ae411b9be922c0
DuongXuanBinh <64977863+DuongXuanBinh@users.noreply.github.com>
2020-06-20 23:28:49 +0700

    ss

---
+++ Môn HTML/Layout/js/boolean-variable.js
@@ -0,0 +1,11 @@
+var intStock;
+var shipping;
+
+intStock=true;
+shipping=false;
+
+var elStock = document.getElementById('stock');
+elStock.className = intStock;
+
+var elShip = document.getElementById('shipping');
+elShip.className=shipping
