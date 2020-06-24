faf34636fee6899605095d2f6c37babb76724a6a
DuongXuanBinh <64977863+DuongXuanBinh@users.noreply.github.com>
2020-06-24 08:03:58 +0700

    a

---
+++ Môn HTML/Layout/js/array-constructor.js
@@ -0,0 +1,3 @@
+var colors = new Array('white','black','custom');
+var el = document.getElementById('colors');
+el.textContent=colors[0];
---
+++ Môn HTML/Layout/js/basic-function.js
@@ -0,0 +1,7 @@
+var msg = 'Sign up to receive our newsletter for 10% off!';
+
+function updateMessage(){
+    var el = document.getElementById('message');
+    el.textContent = msg;
+    }
+updateMessage();
