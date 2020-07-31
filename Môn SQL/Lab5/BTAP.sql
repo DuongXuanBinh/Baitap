313ac3282605e90f6c9107f1f060ef1cef001c00
DuongXuanBinh <64977863+DuongXuanBinh@users.noreply.github.com>
2020-07-31 09:25:47 +0700

    Update BTAP.sql

--- Môn SQL/Lab5/BTAP.sql
+++ Môn SQL/Lab5/BTAP.sql
@@ -3,6 +3,7 @@ GO
 
 USE BookLibraby
 GO
+
 CREATE TABLE Book(
 	BookCode int,
 	BookTitle varchar(100) NOT NULL,
@@ -10,8 +11,11 @@ CREATE TABLE Book(
 	Edition int,
 	BookPrice money,
 	Copies int,
-	CONSTRAINT pk1 PRIMARY KEY (BookCode)
+	CONSTRAINT pk1 PRIMARY KEY(BookCode)
 	)
+GO
+INSERT INTO Book VALUES (1,'Titile1', 'Author1', 2019, 10, 3)
+INSERT INTO Book VALUES (2,'Titile2', 'Author2', 2019, 20, 4)
 GO
 CREATE TABLE Member(
 	MemberCode int,
@@ -21,32 +25,37 @@ CREATE TABLE Member(
 	CONSTRAINT pk2 PRIMARY KEY (MemberCode)
 	)
 GO
+INSERT INTO Member VALUES (1001,'John','NewYork',098213091)
+INSERT INTO Member VALUES (1002,'Jackie','NewYork',012343441)
+GO
+
 CREATE TABLE IssueDetails(
 	BookCode int ,
 	MemberCode int,
 	IssueDate datetime,
 	ReturnDate datetime,
-	CONSTRAINT pk3 foreign key (BookCode) references Book(BookCode),
-	CONSTRAINT pk4 foreign key (MemberCode) references Member(MemberCode),
+	CONSTRAINT fk3 foreign key (BookCode) references Book(BookCode),
+	CONSTRAINT fk4 foreign key (MemberCode) references Member(MemberCode)
 	)
 GO
-
-ALTER TABLE IssueDetails DROP CONSTRAINT pk3
-ALTER TABLE IssueDetails DROP CONSTRAINT pk4
+INSERT INTO IssueDetails VALUES(1,1001,12-8-2020,23-8-2020)
+GO
+ALTER TABLE IssueDetails DROP CONSTRAINT fk3
+ALTER TABLE IssueDetails DROP CONSTRAINT fk4
 GO
 
 ALTER TABLE Book DROP CONSTRAINT pk1
 ALTER TABLE Member DROP CONSTRAINT pk2
 GO
 
-ALTER TABLE Member 
+ALTER TABLE Book 
 	ADD CONSTRAINT pk1 PRIMARY KEY (BookCode)
-ALTER TABLE Book
+ALTER TABLE Member
 	ADD CONSTRAINT pk2 PRIMARY KEY (MemberCode)
 GO
 
-ALTER TABLE IssueDetails ADD CONSTRAINT pk3 FOREIGN KEY (BookCode) REFERENCES Book(BookCode)
-ALTER TABLE IssueDetails ADD CONSTRAINT pk4 FOREIGN KEY (MemberCode) REFERENCES Member(MemberCode)
+ALTER TABLE IssueDetails ADD CONSTRAINT fk3 FOREIGN KEY (BookCode) REFERENCES Book(BookCode)
+ALTER TABLE IssueDetails ADD CONSTRAINT fk4 FOREIGN KEY (MemberCode) REFERENCES Member(MemberCode)
 GO
 
 ALTER TABLE Book
@@ -66,4 +75,11 @@ GO
 ALTER TABLE IssueDetails
 	ADD PRIMARY KEY(BookCode, MemberCode)
 GO
+SELECT * FROM Book
+SELECT * FROM Member
+SELECT * FROM IssueDetails
+
+
+
+
 
